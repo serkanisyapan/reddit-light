4d3768ed2eaefa72b16e111529aed0197174752c
serkanisyapan <serkanisyapan1@hotmail.com>
2023-04-11 12:32:59 +0300

    adds navbar to create post page

--- src/pages/create-post.tsx
+++ src/pages/create-post.tsx
@@ -5,6 +5,7 @@ import { api } from "@/utils/api";
 import { useRouter } from "next/router";
 import { LoadingSpinner } from "@/components/LoadingSpinner";
 import { toast } from "react-hot-toast";
+import { Navbar } from "@/components/Navbar";
 
 type PostInputs = {
   title: string;
@@ -42,43 +43,46 @@ export default function App() {
   };
 
   return (
-    <div className="flex h-screen justify-center">
-      <form
-        className="mt-16 flex w-96 flex-col p-2"
-        // eslint-disable-next-line @typescript-eslint/no-misused-promises
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div>
-          <label className="label">
-            <span className="label-text">title</span>
-          </label>
-          <input
-            type="text"
-            className={`input-primary input w-full ${
-              errors.title ? "input-error" : ""
-            }`}
-            {...register("title")}
-          />
-          {errors.title && <span>This field is required</span>}
-        </div>
-        <label className="label mt-3">
-          <span className="label-text">text</span>
-        </label>
-        <textarea
-          className={`textarea-primary textarea text-base ${
-            errors.content ? "textarea-error" : ""
-          }`}
-          {...register("content", { required: true })}
-        />
-        {errors.content && <span>This field is required</span>}
-        <button
-          disabled={isPosting}
-          className="btn-primary btn mt-5"
-          type="submit"
+    <div className="flex h-full w-full flex-col">
+      <Navbar />
+      <div className="flex h-screen justify-center">
+        <form
+          className="mt-16 flex w-96 flex-col p-2"
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {isPosting ? <LoadingSpinner /> : "Create Post"}
-        </button>
-      </form>
+          <div>
+            <label className="label">
+              <span className="label-text">title</span>
+            </label>
+            <input
+              type="text"
+              className={`input-primary input w-full ${
+                errors.title ? "input-error" : ""
+              }`}
+              {...register("title")}
+            />
+            {errors.title && <span>This field is required</span>}
+          </div>
+          <label className="label mt-3">
+            <span className="label-text">text</span>
+          </label>
+          <textarea
+            className={`textarea-primary textarea text-base ${
+              errors.content ? "textarea-error" : ""
+            }`}
+            {...register("content", { required: true })}
+          />
+          {errors.content && <span>This field is required</span>}
+          <button
+            disabled={isPosting}
+            className="btn-primary btn mt-5"
+            type="submit"
+          >
+            {isPosting ? <LoadingSpinner /> : "Create Post"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
