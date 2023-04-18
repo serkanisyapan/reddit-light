d0a9f8ed41ad41bf09b995fe5a20da9521453da0
serkanisyapan <serkanisyapan1@hotmail.com>
2023-04-18 15:15:15 +0300

    remove post from comments tab on profile

--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -26,7 +26,10 @@ export default function PostComment(props: Comment) {
               width={24}
             />
             <div className="flex items-center gap-2 text-slate-500">
-              <Link href={`/user/${props.username}`}>
+              <Link
+                onClick={(event) => event.stopPropagation()}
+                href={`/user/${props.username}`}
+              >
                 <span className="text-white hover:cursor-pointer hover:underline">
                   u/{props.username}
                 </span>
--- src/components/ProfileFeed.tsx
+++ src/components/ProfileFeed.tsx
@@ -1,5 +1,6 @@
 import useScrollPosition from "@/hooks/useScrollPosition";
 import { api } from "@/utils/api";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { LoadingSpinner, SpinnerContainer } from "./LoadingSpinner";
 import PostComment from "./PostComment";
@@ -7,6 +8,7 @@ import { SinglePost } from "./SinglePost";
 
 export const ProfileFeed = (props: { userId: string; feed: string }) => {
   const scrollPosition = useScrollPosition();
+  const postRouter = useRouter();
   const {
     data,
     isLoading,
@@ -49,11 +51,18 @@ export const ProfileFeed = (props: { userId: string; feed: string }) => {
         );
         return (
           <div className="bg-neutral-focus" key={post.post.id}>
-            <SinglePost {...post} />
-            <hr></hr>
+            {props.feed !== "comments" && <SinglePost {...post} />}
             {props.feed === "comments" && findUserComment && (
-              <div className="ml-8 bg-neutral-focus p-4">
-                <PostComment {...findUserComment} />
+              <div
+                onClick={() => void postRouter.push(`/post/${post.post.id}`)}
+                className="cursor-pointer border-[1px] border-neutral hover:border-white"
+              >
+                <p className="px-12 py-2 text-xs">
+                  {findUserComment.username} commented on {post.post.title}
+                </p>
+                <div className="ml-8 bg-neutral-focus p-4">
+                  <PostComment {...findUserComment} />
+                </div>
               </div>
             )}
           </div>
