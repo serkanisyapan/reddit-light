36b91b74a156c5b744bf6305597cfe4a25241f89
serkanisyapan <serkanisyapan1@hotmail.com>
2023-04-19 18:33:08 +0300

    feat: add profile name to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { PlusIcon } from "./Icons";
 
 export const Navbar = () => {
   const { user, isSignedIn } = useUser();
+  const router = useRouter();
+  const profileName = router.query?.slug;
 
   let userContent;
   if (!user || !user?.username || !isSignedIn) {
@@ -49,20 +52,27 @@ export const Navbar = () => {
   }
 
   return (
-    <div className="navbar flex min-h-[70px] justify-between bg-primary text-primary-content">
-      <Link
-        href="/"
-        className="flex flex-row items-center gap-2 text-xl normal-case"
-      >
-        <Image
-          className="box-border rounded-full border-[2px] border-[#df744a] object-cover"
-          src="/redditlight.png"
-          alt="Reddit Light"
-          width={52}
-          height={52}
-        />
-        <span className="invisible md:visible">Reddit Light</span>
-      </Link>
+    <div className="navbar flex min-h-[70px] items-center justify-between bg-primary text-primary-content">
+      <div>
+        <Link
+          href="/"
+          className="flex flex-row items-center gap-2 text-xl normal-case"
+        >
+          <Image
+            className="box-border rounded-full border-[2px] border-[#df744a] object-cover"
+            src="/redditlight.png"
+            alt="Reddit Light"
+            width={52}
+            height={52}
+          />
+          <span className="hidden md:block">Reddit Light</span>
+        </Link>
+        {profileName && (
+          <span className="ml-3 hidden text-base sm:block">
+            u/{profileName}
+          </span>
+        )}
+      </div>
       <div className="flex-none gap-2">
         <div>{userContent}</div>
       </div>
