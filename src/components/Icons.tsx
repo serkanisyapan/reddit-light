83803961625c1e1901a6efc7acc60b3a47cc64ef
serkanisyapan <serkanisyapan1@hotmail.com>
2023-04-23 17:59:15 +0300

    fix: accessibility issues

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,20 +1,22 @@
 export const PlusIcon = () => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-plus"
-    >
-      <line x1="12" y1="5" x2="12" y2="19"></line>
-      <line x1="5" y1="12" x2="19" y2="12"></line>
-    </svg>
+    <button aria-label="createpost">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="feather feather-plus"
+      >
+        <line x1="12" y1="5" x2="12" y2="19"></line>
+        <line x1="5" y1="12" x2="19" y2="12"></line>
+      </svg>
+    </button>
   );
 };
 
@@ -112,6 +114,7 @@ export const UpvoteIcon = ({
 }: VoteProps) => {
   return (
     <button
+      aria-label="upvote"
       onClick={(event) => {
         event.stopPropagation();
         if (isVoting) return;
@@ -144,6 +147,7 @@ export const DownvoteIcon = ({
 }: VoteProps) => {
   return (
     <button
+      aria-label="downvote"
       onClick={(event) => {
         event.stopPropagation();
         if (isVoting) return;
