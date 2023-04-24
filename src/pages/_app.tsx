f69275562bdcfc7acf0209f902b7a20caafc70ed
serkanisyapan <serkanisyapan1@hotmail.com>
2023-04-24 20:42:27 +0300

    refactor: better meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,16 @@ import { api } from "@/utils/api";
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Reddit Light</title>
+        <meta name="description" content="Posts of posts 💯" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <main className="flex flex-col items-center" data-theme="forest">
         <Component {...pageProps} />
